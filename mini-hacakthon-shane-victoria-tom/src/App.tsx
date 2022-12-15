7c26949b040636c143c07969cbcf7b26f95c8217
BushraFatimaBF <vglampard@protonmail.com>
2022-12-15 12:33:09 +0000

    erro hunting, maybe the added gender API IS in fact working? We can test htis by pushing forward and seeing if it renders immediately or not lol

--- mini-hacakthon-shane-victoria-tom/src/App.tsx
+++ mini-hacakthon-shane-victoria-tom/src/App.tsx
@@ -5,20 +5,41 @@ import { ImageDisplay } from "./components/ImageDisplay/ImageDisplay";
 
 function App() {
   const [age, setAge] = useState(0);
+  const [gender, setGender] = useState((""))
   const [name, setName] = useState("");
 
   function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     setName(e.target.value);
   }
 
+async function getAge(){
+  console.log("age? yeas");
+  const result = await fetch(`https://api.agify.io/?name=${name}`);
+  const data = await result.json();
+  return data.age;
+}
+
+async function getGender(){
+  console.log("Gender? yeas");
+  const result = await fetch(`https://api.genderize.io?name=${name}`);
+  const data = await result.json();
+  console.log("data gender api:", data.gender)
+  
+  return data.gender;
+ 
+}
+
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log("yeas");
-    const result = await fetch(`https://api.agify.io/?name=${name}`);
-    const data = await result.json();
-    setAge(data.age);
-    console.log("is it a number?:", data.age);
-    console.log(data);
+    console.log("handlesubmit yeas");
+    const genderData = await getGender();
+    console.log("gender at handelsubmit:", genderData)
+    const ageData = await getAge();
+    console.log("age at handelsubmit:", ageData)
+   setAge(ageData);
+   setGender(genderData);
+   console.log("gender after setstate:", gender)
+   console.log("age after setstate:", age)
   }
 
   return (
